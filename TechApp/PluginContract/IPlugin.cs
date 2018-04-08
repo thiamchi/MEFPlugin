9a59de1f2d6d0edadee48b6cb7a44f7a615b4cb6
thiamchi <35895671+thiamchi@users.noreply.github.com>
2018-04-08 22:39:28 +0800

    Setup Hello World Plugin

---
+++ TechApp/PluginContract/IPlugin.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PluginContract
+{
+    public interface IPlugin
+    {
+        string PluginName { get;}
+        void run();
+    }
+}
